0e155ba52fd7e920147d3ec4ce5ba0f47578ea29
carloo07 <76703454+carloo07@users.noreply.github.com>
2021-12-03 18:31:12 +0300

    Update invite.js

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -12,7 +12,7 @@ module.exports = {
   run: async (bot, message, args, dev) => {
     let embed = new Discord.MessageEmbed()
       .setColor(Color)
-      .setTitle("**Bobo Bot**  InviteLink!")
+      .setTitle("**Gamble Bot**  InviteLink!")
       .setURL(
         `https://discord.com/api/oauth2/authorize?client_id=758821724996370473&permissions=8&scope=bot`
       )
