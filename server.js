9962e2824e43ef0f030c82922accfaf10121788a
carloo07 <76703454+carloo07@users.noreply.github.com>
2021-12-04 01:21:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -86,7 +86,7 @@ bot.on("ready", () => {
 });
 
 bot.on("ready", () => {
-  bot.user.setActivity("Ghelp | Updated: 1.0.0V", { type: "COMPETING" });
+  bot.user.setActivity("Ghelp | Updated: 1.0.0V", { type: "STREAMING" });
 });
 ///////////
 
