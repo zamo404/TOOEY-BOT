fc7fb27b5ed04fc5d9db74e762731491cb468616
Alex-DevV <88611334+Alex-DevV@users.noreply.github.com>
2022-01-16 14:52:35 +0300

    Update balance.js

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -19,13 +19,13 @@ module.exports = {
       let autho = await User.findOne({ userID: member.id });
       message.channel.send({
         content:`
-          <:alex_40:932239945328914433> **${member.username}**, credits balance is __${autho.money}__ ${m}`
+          <:alex_40:932239945328914433> **${member.username}**, credits balance is __${autho,.money}__ ${m}`
       });
     }
     if (!member) {
       let author = await User.findOne({ userID: message.author.id });
       message.reply({
-        content: `<:alex_40:932239945328914433> **${message.author.username}**, Your credits balance is:  __\`${author.money}\`__${m}
+        content: `<:alex_40:932239945328914433> **${message.author.username}**, Your credits balance is:  __\`${author,.money}\`__${m}
       `});
     }
   }
