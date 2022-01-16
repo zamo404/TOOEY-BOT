ac869f2fca3430f83a766595ebf5857a8157686a
Alex-DevV <88611334+Alex-DevV@users.noreply.github.com>
2022-01-16 15:15:53 +0300

    Update owner.js

--- data/owner.js
+++ data/owner.js
@@ -2,4 +2,4 @@ const schema = mongoose.Schema({
     ownerCode: String,
     worldWhitelist: { type: Array, default: [] }
 });
-module.exports = mongoose.model("Owner", schema)
+module.exports = mongoose.model("dev", schema)
