7d19adea772a9a191f062d9ddd968221652203d3
Alex-DevV <88611334+Alex-DevV@users.noreply.github.com>
2022-01-16 14:40:50 +0300

    Update daily.js

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -19,7 +19,7 @@ module.exports = {
 
       let DR = Math.floor(Math.random() * 2000) + 1000
       
-      message.channel.send({content:`**${message.author.username}** you get 💰$\`${DR}\` credits`})
+      message.channel.send({content:`**${message.author.username}** you get <:alex_35:932236986041909268> $\`${DR}\` credits`})
       
       data.time = Date.now();
       data.name = message.author.username
