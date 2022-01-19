0ef436a17fea3b0a929c0680ce799c1558881bd9
ANOxDevV <88611334+ANOxDevV@users.noreply.github.com>
2022-01-19 19:02:31 +0300

    Update guild.js

--- data/guild.js
+++ data/guild.js
@@ -1,7 +1,7 @@
 const schema = mongoose.Schema({
     guildID: String,
     ///user:String,
-    prefix: { type: String, default: "T"},
+    prefix: { type: String, default: "K"},
     
 });
 module.exports = mongoose.model("Guild", schema)
