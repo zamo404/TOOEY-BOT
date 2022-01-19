b39174cc3317b8da852dc7a6a436846f2accd6bc
Zamoxe82 <96996249+Zamoxe82@users.noreply.github.com>
2022-01-19 17:15:12 +0300

    Update invite.js

--- commands/general/invite.js
+++ commands/general/invite.js
@@ -12,9 +12,9 @@ module.exports = {
   run: async (bot, message, args, dev) => {
     let embed = new Discord.MessageEmbed()
       .setColor(Color)
-      .setTitle("**Gamble Bot**  InviteLink!")
+      .setTitle("**Tooey Bot**  InviteLink!")
       .setURL(
-        `https://discord.com/api/oauth2/authorize?client_id=758821724996370473&permissions=8&scope=bot`
+        `https://discord.com/api/oauth2/authorize?client_id=930409896199991316&permissions=8&scope=bot`
       )
      
       
