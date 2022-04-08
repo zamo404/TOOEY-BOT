b28826d3a740e59079bde99cad4de6593bf75bc2
zamo404 <96996249+zamo404@users.noreply.github.com>
2022-04-09 02:33:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -95,4 +95,4 @@ bot.on("messageCreate", async message => {
 
  
 
-bot.login("OTMwNDA5ODk2MTk5OTkxMzE2.Yd1dvQ.pIqAqFJzEqns2FoG4jGsPUnU8eQ");
+bot.login("OTUxMTkyNDMzNzY3NjI0NzM0.Yij4_A.Gm3x78-YsnxfSfE8DJo0Y8NC_l8");
